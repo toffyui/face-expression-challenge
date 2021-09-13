5c2a4d65f317663fdf23a7634a7876091cf2f81a
yuikoito <yuiko.dev@gmail.com>
2021-09-14 06:23:25 +0900

    fix meta

--- pages/index.tsx
+++ pages/index.tsx
@@ -255,10 +255,14 @@ export default function Home() {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta property="og:url" content={process.env.NEXT_PUBLIC_BASE_URL} />
         <meta name="description" content={t.metaDesc} />
+        <meta property="og:title" content={t.title} />
+        <meta property="og:type" content="website" />
+        <meta property="og:description" content={t.metaDesc} />
         <meta property="og:site_name" content={t.title} />
         <meta property="og:image" key="ogImage" content="/emojis/happy.png" />
         <link rel="icon" href="/emojis/happy.png" />
         <meta name="twitter:site" content="@yui_active" />
+        <meta name="twitter:creator" content="@yui_active" />
       </Head>
       <main className={styles.main}>
         <div className={styles.videoContainer}>
